79df4bcbebf225a86168d8986fc40bf97e7e713f
Harsh Dalvi <harshcasual44@gmail.com>
2026-01-14 00:56:40 +0530

    Update auth.js
    production updates

--- auth.js
+++ auth.js
@@ -21,7 +21,8 @@ passport.use(new GoogleStrategy (
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      callbackURL: `${process.env.SERVER_URL}/auth/google/callback`,
+
     },
 
     async(acessToken, refreshToken, profile, done)=>
@@ -68,3 +69,4 @@ passport.deserializeUser(async (id, done) =>
 
 
 export {passport, db};
+
